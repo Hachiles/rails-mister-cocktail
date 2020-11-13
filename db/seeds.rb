d879e37130f20774871e4ae3c105daede2785192
Ulysse Bondy <ulysse.bondy@hotmail.com>
2020-11-13 11:37:24 +0100

    Pre-heroku commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+puts "Cleaning database now..."
+Ingredient.destroy_all
+puts "Database clean ✅"
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+serialized = open(url).read
+list = JSON.parse(serialized)
+
+list_array = list["drinks"]
+list_array.each do |ing|
+  Ingredient.create(name: ing["strIngredient1"])
+end
+
+puts "Done! Created #{Ingredient.count} ingredients."
